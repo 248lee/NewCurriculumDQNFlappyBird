1581dad1a6fbbe556e1e739b25da195a4c56893a
248lee <56167291+248lee@users.noreply.github.com>
2024-01-13 11:30:32 +0800

    Update non_gui.py

--- non_gui.py
+++ non_gui.py
@@ -1,2 +1,2 @@
 from deep_q_network import trainNetwork
-trainNetwork(1, 2, lock_mode=0, is_simple_actions_locked=False, is_activate_boss_memory=False, max_steps=2010, isSweetBoss=True, resume_Adam=False, learning_rate=3e-6, event=None, is_colab=True)
+trainNetwork(1, 2, lock_mode=0, is_simple_actions_locked=False, is_activate_boss_memory=False, max_steps=200000, isSweetBoss=True, resume_Adam=False, learning_rate=3e-6, event=None, is_colab=True)
